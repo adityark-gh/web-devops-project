db193142e42e4224ae9eaf5ed67928d008d756df
Aditya Ramakrish <aditya.ramakrish@gmail.com>
2026-05-23 18:11:05 +0000

    junit import

--- src/test/java/com/example/MessageServiceTest.java
+++ src/test/java/com/example/MessageServiceTest.java
@@ -1,8 +1,7 @@
 
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import junit.framework.TestCase;
 
 public class MessageServiceTest {
 
